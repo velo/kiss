3954d653fc2f4ff33059cc2684b390ebb55121c8
Marvin Froeder <marvin@contaazul.com>
2013-06-16 14:08:36 -0300

    Code format

--- kiss-querydsl-mockery/src/main/java/com/marvinformatics/kiss/querydslmockery/impl/MockeryQueryTemplates.java
+++ kiss-querydsl-mockery/src/main/java/com/marvinformatics/kiss/querydslmockery/impl/MockeryQueryTemplates.java
@@ -3,7 +3,6 @@ package com.marvinformatics.kiss.querydslmockery.impl;
 import com.mysema.query.collections.CollQueryTemplates;
 import com.mysema.query.types.Ops;
 
-
 /**
  * @author Marvin
  */
@@ -13,6 +12,7 @@ public class MockeryQueryTemplates extends CollQueryTemplates {
 
 	protected MockeryQueryTemplates() {
 		super();
+
 		String functions = MockeryQueryFunctions.class.getName();
 		add(Ops.EQ, functions + ".equals({0}, {1})");
 		add(Ops.NE, "!" + functions + ".equals({0}, {1})");
